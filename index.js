4e4db018baaf7bec05af6f202eb9e67fc6c95a3a
dedenbangkit <mail@dedenbangkit.com>
2017-12-21 21:29:11 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app     = express();
+var path    = require("path");
+
+app.get('/',function(req,res){
+      res.sendFile(path.join(__dirname+'/index.html'));
+    });
+
+app.listen(3000);
+console.log("MangaFreak berjalan di port 3000");
